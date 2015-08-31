7bd780f3ee0d39fafc29a61d11de67908bd5fa6b
Stefan Falkner <sfalkner@informatik.uni-freiburg.de>
2015-08-31 21:01:59 +0200

    started a unit test for the regression forest

---
+++ rfr/src/tests/unit_test_regression_forest.cpp
@@ -0,0 +1,35 @@
+// compile with the following two options:
+// -lboost_unit_test_framework -DBOOST_TEST_DYN_LINK
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE rfr_test
+#include <boost/test/unit_test.hpp>
+
+#include <random>
+
+#include "data_containers/mostly_continuous_data_container.hpp"
+#include "splits/binary_split_one_feature_rss_loss.hpp"
+
+
+typedef std::default_random_engine rng_type;
+typedef double num_type;
+
+
+
+typedef double rng_type;
+
+BOOST_AUTO_TEST_CASE( data_container_tests ){
+	rfr::mostly_contiuous_data<num_type> data;
+
+    char *filename = (char*) malloc(1024*sizeof(char));
+
+    strcpy(filename, boost::unit_test::framework::master_test_suite().argv[1]);
+    strcat(filename, "diabetes_features.csv");
+    data.read_feature_file(filename);
+
+    strcpy(filename, boost::unit_test::framework::master_test_suite().argv[1]);
+    strcat(filename, "diabetes_responses.csv");
+    data.read_response_file(filename);
+
+
+    free(filename);
+}
