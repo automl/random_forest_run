f0473e4803185c1cb68c90966ca8747b514c06f4
Stefan Falkner <sfalkner@informatik.uni-freiburg.de>
2015-09-08 17:16:06 +0200

    working on the python interface

--- rfr/src/python_module/rfr.cpp
+++ rfr/src/python_module/rfr.cpp
@@ -1,7 +1,7 @@
-//#include "rfr/forest/regression_forest.hpp"
+#include <iostream>
+#include <random>
 
 #include <boost/numpy.hpp>
-
 
 #include "rfr/splits/binary_split_one_feature_rss_loss.hpp"
 #include "rfr/nodes/temporary_node.hpp"
@@ -18,13 +18,13 @@ typedef double response_type;
 typedef unsigned int index_type;
 typedef std::default_random_engine rng_type;
 
-typedef rfr::mostly_contiuous_data<num_type, response_type, index_type> data_container_type;
-
 typedef rfr::binary_split_one_feature_rss_loss<rng_type, num_type, response_type, index_type> split_type;
 typedef rfr::k_ary_node<2, split_type, rng_type, num_type, response_type, index_type> node_type;
 typedef rfr::temporary_node<num_type, index_type> tmp_node_type;
 
 typedef rfr::k_ary_random_tree<2, split_type, rng_type, num_type, response_type, index_type> tree_type;
+typedef rfr::regression_forest< tree_type, rng_type, num_type, response_type, index_type> forest_type;
+
 
 
 
@@ -71,21 +71,85 @@ char const* print_array( boost::numpy::ndarray const & array){
 
 
 
+void check_array ( boost::numpy::ndarray const & array, int num_dims){
+    if (array.get_dtype() != boost::numpy::dtype::get_builtin<double>()) {
+        PyErr_SetString(PyExc_TypeError, "Incorrect array data type");
+        boost::python::throw_error_already_set();
+    }
+    if (array.get_nd() != num_dims) {
+        PyErr_SetString(PyExc_TypeError, "Incorrect number of dimensions");
+        boost::python::throw_error_already_set();
+    }
+    if (!(array.get_flags() & boost::numpy::ndarray::C_CONTIGUOUS)) {
+        PyErr_SetString(PyExc_TypeError, "Array must be row-major contiguous");
+        boost::python::throw_error_already_set();
+    }
+}
+
+
+
 
 
 class binary_regression_forest_rss_split{
   private:
-    tree_type the_tree;
+    
+    rfr::forest_options<num_type, response_type, index_type> forest_opts;
+    std::default_random_engine rng;
+    forest_type the_forest;
 
   public:
 
-    // constructor
-    binary_regression_forest_rss_split(){
+    binary_regression_forest_rss_split(	unsigned int num_trees,
+					unsigned int seed = 0,
+   					bool do_bootstrapping = false,
+					unsigned int num_data_points_per_tree = 0,
+					unsigned int max_num_nodes = 0,
+					unsigned int max_depth = 0,
+					unsigned int max_features_per_split = 0,
+					unsigned int min_samples_to_split = 0,
+					unsigned int min_samples_in_leaf = 0,
+					response_type epsilon_purity = -1
+					){
 
-	rfr::tree_options tree_opts();
-	rfr::forest_options forest_opts(tree_opts);
+	if (seed > 0) rng.seed(seed);
 
-	the_tree = tree_type (tree_opts);
+	// store all the tree related options
+	rfr::tree_options<num_type, response_type, index_type> to;
+	if (max_depth > 0) to.max_depth = max_depth;
+	if (max_features_per_split > 0) to.max_features = max_features_per_split;
+	if (min_samples_to_split > 0) to.min_samples_to_split = min_samples_to_split;
+	if (min_samples_in_leaf > 0) to.min_samples_in_leaf = min_samples_in_leaf;
+	if (max_num_nodes >  0) to.max_num_nodes = max_num_nodes;
+	if (epsilon_purity >= 0) to.epsilon_purity = epsilon_purity;
+
+
+	// now the forest related options
+	rfr::forest_options<num_type, response_type, index_type> forest_opts (to);
+	forest_opts.num_trees = num_trees;
+	forest_opts.do_bootstrapping = do_bootstrapping;
+	if (num_data_points_per_tree > 0) forest_opts.num_data_points_per_tree = num_data_points_per_tree;
+
+	forest_type the_forest(forest_opts);
+	
+
+    }
+					
+
+    void fit (	boost::numpy::ndarray const & features,
+		boost::numpy::ndarray const & responses,
+		boost::numpy::ndarray const & types
+		){
+
+	// simple sanity checks
+	check_array(features, 2);
+	check_array(responses, 1);
+	check_array(types, 1);
+
+	// create the data container
+
+
+	
+	
 
     }
 
@@ -95,13 +159,9 @@ class binary_regression_forest_rss_split{
 
 
 
-
-
-
 #include <boost/python.hpp>
 
-BOOST_PYTHON_MODULE(librfr){
+BOOST_PYTHON_MODULE(rfr){
 	boost::numpy::initialize();
-	boost::python::def("greet", greet);
 	boost::python::def("print_array", print_array);
 }
