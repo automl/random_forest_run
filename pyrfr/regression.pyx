c3dd0c0457a34ae5307bb0e39048305e09ce978d
Krxsy <wunschc@informatik.uni-freiburg.de>
2015-11-01 19:49:11 +0100

    newest version docs pyrfr

--- pyrfr/regression.pyx
+++ pyrfr/regression.pyx
@@ -26,18 +26,24 @@ ctypedef  k_ary_random_tree[two, binary_split_one_feature_rss_loss[rng_t, num_t,
 Base classes
 """
 cdef class data_base:
-	""" base class for the data container to reuse as much code as possible"""
+	""" 
+	base class for the data container to reuse as much code as possible
+	"""
 	cdef data_container_base[num_t,response_t, index_t] *thisptr
 
 	def __dealloc__(self):
 		del self.thisptr
 
 	def num_features(self):
-		""" the number of features of the data"""
+		""" 
+		the number of features of the data
+		"""
 		return self.thisptr.num_features()
 
 	def num_data_points(self):
-		""" the number of data points in the container """
+		""" 
+		the number of data points in the container 
+ 		"""
 		return self.thisptr.num_data_points()
 
 	def set_type_of_feature(self, index_t fi, index_t ft):
@@ -58,7 +64,9 @@ cdef class data_base:
 		return self.thisptr.retrieve_data_point(new_i)
 
 	def retrieve_response(self, int index):
-		""" return a point in the data """
+		""" 
+		return a point in the response data 
+		"""
 		while index < 0:
 			index += self.thisptr.num_data_points()
 		# to avoid:
@@ -74,18 +82,22 @@ cdef class data_base:
 	def export_responses(self):
 		return np.array([ self.thisptr.response(i) for i in range(self.num_data_points())])
 
-######################################################
-# The data containers available in the python module #
-######################################################
+"""
+ The data containers available in the python module 
+"""
 cdef class numpy_data_container(data_base):
-	""" A data container wrapping three numpy arrays"""
+	""" 
+	A data container wrapping three numpy arrays
+	"""
 
 	cdef public object features
 	cdef public object responses
 	cdef public object types
 
 	def __cinit__(self, np.ndarray[num_t,ndim=2] feats, np.ndarray[response_t,ndim=1] resp, np.ndarray[index_t] types):
-		""" constructor should make no copy if the data comes in C-contiguous form."""
+		""" 
+		constructor should make no copy if the data comes in C-contiguous form.
+		"""
 		# store a 'reference' so that the numpy array does not get garbage collected
 		# also, assure the data is contiguous in memory
 		self.features = np.ascontiguousarray(feats)
@@ -95,7 +107,9 @@ cdef class numpy_data_container(data_base):
 
 
 	def add_data_point(self, np.ndarray[num_t,ndim=1] fs, response_t r):
-		""" adds a data point by creating new python arrays, thus copies the data"""
+		""" 
+		adds a data point by creating new python arrays, thus copies the data
+		"""
 		assert fs.shape[0] == self.features.shape[1]
 
 		# create new numpy arrays with the extended data
@@ -110,7 +124,9 @@ cdef class numpy_data_container(data_base):
 		self.thisptr = new array_data_container[num_t,response_t, index_t] (&feats[0,0], &resp[0], &types[0], feats.shape[0], feats.shape[1])
 
 cdef class mostly_continuous_data_container(data_base):
-	""" A python wrapper around the C++ data container for mostly continuous data."""
+	""" 
+	A python wrapper around the C++ data container for mostly continuous data.
+	"""
 
 	def __cinit__(self, int num_features):
 		self.thisptr = new mostly_continuous_data[num_t,response_t, index_t] (num_features)
@@ -124,16 +140,19 @@ cdef class mostly_continuous_data_container(data_base):
 
 
 
-######################
-# The actual forests #
-######################
+
 cdef class regression_forest_base:
+	"""
+ 	The actual forests
+	"""
 	# attributes for the forest parameters
 	cdef public index_t num_trees
 	cdef public index_t num_data_points_per_tree
 	cdef public bool do_bootstrapping
 
-	# attributes for the individual trees
+
+	#attributes for the individual trees.
+
 	cdef public index_t max_features
 	cdef public index_t max_depth
 	cdef public index_t max_num_nodes
@@ -141,11 +160,16 @@ cdef class regression_forest_base:
 	cdef public index_t min_samples_in_leaf
 	cdef public response_t epsilon_purity
 
-	# to (re)seed the rng
+
+	#to (re)seed the rng.
+
 	cdef public index_t seed
 
 	cdef rng_t *rng_ptr
-	# pointer to the (abstract) C++ class
+
+	#pointer to the (abstract) C++ class. 
+
+
 	#cdef regression_forest_base_t * forest_ptr
 
 	def __init__(self):
@@ -170,7 +194,7 @@ cdef class regression_forest_base:
 		raise NotImplementedError
 
 	cdef forest_options[num_t, response_t, index_t] build_forest_options(self, data_base data):
-
+				
 		cdef tree_options[num_t, response_t, index_t] to
 		to.max_features = self.max_features if self.max_features > 0 else data.num_features()
 		to.max_depth = self.max_depth
@@ -180,7 +204,9 @@ cdef class regression_forest_base:
 		to.epsilon_purity = self.epsilon_purity
 
 
-		#construct the forest option object
+
+		#construct the forest option object.
+
 		cdef forest_options[num_t, response_t, index_t] fo
 
 		fo.num_trees=self.num_trees
@@ -188,7 +214,9 @@ cdef class regression_forest_base:
 		fo.do_bootstrapping = self.do_bootstrapping
 		fo.tree_opts = to
 
-		#reseed the rng if needed
+
+		#reseed the rng if needed.
+
 		if (self.seed > 0):
 			self.rng_ptr.seed(self.seed)
 			self.seed=0
@@ -197,6 +225,9 @@ cdef class regression_forest_base:
 
 
 cdef class binary_rss(regression_forest_base):
+	"""
+	The random forest regressor. Builds a forest of trees from the training set (X,y) and predicts the regression targets for X. The splits are made by calculating the residual sum of squares and taking the min.
+	"""
 	cdef regression_forest[ binary_rss_tree_t, rng_t, num_t, response_t, index_t]* forest_ptr
 	
 	def __init(self):
@@ -204,7 +235,7 @@ cdef class binary_rss(regression_forest_base):
 	
 	def __dealloc__(self):
 		del self.forest_ptr
-	
+
 	def fit(self, data_base data):
 		del self.forest_ptr
 		fo = self.build_forest_options(data)
