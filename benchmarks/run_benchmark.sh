b25e89de80cb7a5241f3bd8b6c148eb2c9ee3fe1
Stefan Falkner <sfalkner@informatik.uni-freiburg.de>
2015-11-11 13:32:09 +0100

    about to close the benchmark branch

--- benchmarks/run_benchmark.sh
+++ benchmarks/run_benchmark.sh
@@ -1,11 +1,22 @@
-O_level=2
-num_feats=1000
-num_datapoints=500000
-num_samples=5
+O_level=$1
+num_feats=100
+num_datapoints=100000
+num_samples=50000
+num_trees=64
 
-g++ -I../include/ -O$O_level -Wall -o benchmark_sorting -std=c++11 benchmark_sorting.cpp
-./benchmark_sorting $num_feats $num_datapoints $num_samples
+#g++ -I../include/ -O$O_level -Wall -o benchmark_sorting -std=c++11 benchmark_sorting.cpp
+#./benchmark_sorting $num_feats $num_datapoints $num_samples
 
-g++ -I../include/ -O$O_level -Wall -o benchmark_sorting_profile -std=c++11 benchmark_sorting.cpp
-./benchmark_sorting_profile $num_feats $num_datapoints $num_samples
-gprof benchmark_sorting_profile gmon.out > analysis.txt
+#g++ -I../include/ -O$O_level -Wall -o benchmark_sorting_profile -std=c++11 benchmark_sorting.cpp
+#./benchmark_sorting_profile $num_feats $num_datapoints $num_samples
+#gprof benchmark_sorting_profile gmon.out > analysis.txt
+
+g++ -I../include/ -O$O_level -Wall -pg -o benchmark_rss_v1 -std=c++11 benchmark_rss_v1.cpp
+time ./benchmark_rss_v1 $num_feats $num_datapoints $num_samples $num_trees
+gprof benchmark_rss_v1 gmon.out > analysis_v1.txt
+gprof benchmark_rss_v1 | python /home/sfalkner/.local/lib/python3.5/site-packages/gprof2dot.py -s | dot -Tpng -o graph_v1.png
+
+g++ -I../include/ -O$O_level -Wall -pg -o benchmark_rss_v2 -std=c++11 benchmark_rss_v2.cpp
+time ./benchmark_rss_v2 $num_feats $num_datapoints $num_samples $num_trees
+gprof benchmark_rss_v2 gmon.out > analysis_v2.txt
+gprof benchmark_rss_v2 | python /home/sfalkner/.local/lib/python3.5/site-packages/gprof2dot.py -s | dot -Tpng -o graph_v2.png
