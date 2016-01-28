61b2ee0e0cf502a7ecff83d6c189efe916017c93
Joel Kaiser <joel@renumi.de>
2016-01-28 13:51:02 +0100

    corrected math in predict_mean_var_of_mean_response_on_set

--- include/rfr/forests/regression_forest.hpp
+++ include/rfr/forests/regression_forest.hpp
@@ -7,6 +7,7 @@
 #include <utility>
 #include <cmath>
 #include <numeric>
+#include <tuple>
 #include <random>
 #include <algorithm>
 #include <functional>  // std::bind
@@ -180,32 +181,33 @@ class regression_forest{
 	 * \param set_features a 1d-array containing the (partial) assignments used for the averaging. Every NaN value will be replaced by the corresponding value from features. The array must hold set_size times the number of features entries! There is no consistency check!
 	 * \param set_size number of feature vectors in set_features
 	 * 
-	 * \return std::pair<num_type, num_type> mean and variance prediction of a feature vector averaged over 
+	 * \return std::pair<num_type, num_type> mean and variance of empirical mean prediction of a feature vector averaged over 
 	 */
 	std::pair<num_type, num_type> predict_mean_var_of_mean_response_on_set (num_type *features, num_type* set_features, index_type set_size){
 
-		num_type fv[num_features];
-		
-		rfr::running_statistics<num_type> mean_stats, var_stats;
-		
-		for (auto &t : the_trees){
+			num_type fv[num_features];
 
-			rfr::running_statistics<num_type> tree_mean_stats, tree_var_stats;
-			
-			for (auto i=0u; i < set_size; ++i){
-			
-				rfr::merge_two_vectors(features, &set_features[i*num_features], fv, num_features);
+			rfr::running_statistics<num_type> mean_stats, var_stats;
 
-				num_type m , v;	index_type n;
-				std::tie(m, v, n) = t.predict_mean_var_N(fv);
-				
-				tree_mean_stats(m); tree_var_stats(v);
+			for (auto &t : the_trees){
+
+					rfr::running_statistics<num_type> tree_mean_stats, tree_var_stats;
+
+					for (auto i=0u; i < set_size; ++i){
+
+							rfr::merge_two_vectors(features, &set_features[i*num_features], fv, num_features);
+
+							num_type m , v; index_type n;
+							std::tie(m, v, n) = t.predict_mean_var_N(fv);
+
+							tree_mean_stats(m); tree_var_stats(v);
+					}
+
+					mean_stats(tree_mean_stats.mean());
+					var_stats(std::max<num_type>(0, tree_var_stats.mean()/set_size));
 			}
 			
-			mean_stats(tree_mean_stats.mean());
-			var_stats(std::max<num_type>(0,tree_mean_stats.variance() + tree_var_stats.mean()));
-		}	
-		return(std::pair<num_type, num_type> (mean_stats.mean(), std::max<num_type>(0, mean_stats.variance() + var_stats.mean()) ));
+			return(std::pair<num_type, num_type>(mean_stats.mean(), std::max<num_type>(0, mean_stats.variance()) + std::max<num_type>(0, var_stats.mean())));
 	}
 
 
