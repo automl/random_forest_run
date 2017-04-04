789ce01931c5331e2186e7dc8ca40ef0cae07601
Stefan Falkner <sfalkner@cs.uni-freiburg.de>
2017-04-04 09:06:32 +0200

    updated pyrfr_example to show all_leaf_values

--- tests/pyrfr_example.py
+++ tests/pyrfr_example.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("../build")
+sys.path.append("../build/")
 import os
 here = os.path.dirname(os.path.realpath(__file__))
 
@@ -14,8 +14,12 @@ import pyrfr.regression as reg
 
 
 data_set_prefix = '%(here)s/../test_data_sets/diabetes_' % {"here":here}
-data = reg.data_container()
+data = reg.default_data_container(10)
 data.import_csv_files(data_set_prefix+'features.csv', data_set_prefix+'responses.csv')
+
+
+
+
 
 
 rng = reg.default_random_engine()
@@ -24,7 +28,6 @@ rng = reg.default_random_engine()
 
 
 the_forest = reg.binary_rss_forest()
-
 the_forest.options.num_trees = 64
 the_forest.options.num_data_points_per_tree = 200
 
@@ -40,7 +43,6 @@ num_datapoints_old = the_forest.options.num_data_points_per_tree
 
 
 # loading it works like that
-
 the_forest = reg.binary_rss_forest()
 the_forest.load_from_binary_file("/tmp/pyrfr_test.bin")
 
@@ -53,81 +55,17 @@ feature_vector = data.retrieve_data_point(0)
 print(feature_vector)
 print(the_forest.predict(feature_vector))
 
-exit(0)
-
 # it is possible to get the actual response values from the corresponding
 # leaf in each tree that the given feature vector falls into
 # The method returns a nested list
-print(the_forest.all_leaf_values(features[0]))
+print(the_forest.all_leaf_values(feature_vector))
 
 # quantile regression forest estimations
-#alphas =[0.25, 0.5, 0.75]
-#print(the_forest.quantile_rf(features[0], alphas))
+the_forest = reg.qr_forest()
+the_forest.options.num_trees = 64
+the_forest.options.num_data_points_per_tree = 200
 
-
-# let's play around a bit and train different numbers of trees and compare the speed to scikit learn
-# Note: the dataset here is pretty small, so the results may not by representative
-times_rfr=[]
-times_scikit=[]
-num_trees = [1, 2, 4, 8, 16, 32, 64, 128]
-
-for nt in num_trees:
-	print("training {} trees".format(nt))
-	the_forest.num_trees=nt
-
-	start = time.time()
-
-	# this is how you fit the forest, it should work with any data container
-	the_forest.fit(data2)
-
-	end = time.time()
-	times_rfr.append(end-start)
-
-	sk_forest = RandomForestRegressor(nt, max_features=the_forest.max_features)
-	start = time.time()
-	sk_forest.fit(features, responses)
-	end = time.time()
-	times_scikit.append(end-start)
-
-
-predictions_rfr = np.zeros_like(responses);
-predictions_scikit = np.zeros_like(responses);
-
-
-
-for i in range(features.shape[0]):
-	sample = features[i]
-	predictions_rfr[i] = the_forest.predict(sample)[0]
-	predictions_scikit[i] = sk_forest.predict(sample)
-
-
-
-plt.plot(predictions_rfr, label="rfr predictions")
-plt.plot(predictions_scikit, label="scikit predictions")
-plt.plot(responses, label="responses")
-plt.xlabel("index")
-plt.ylabel("response value")
-
-
-plt.legend()
-
-
-plt.figure()
-plt.plot(np.sort((responses-predictions_rfr)/responses), label="rfr")
-plt.plot(np.sort((responses-predictions_scikit)/responses), label="scikit")
-plt.xlabel("sorted index")
-plt.ylabel("relative error")
-
-
-plt.legend()
-plt.figure()
-
-
-
-plt.xlabel("number of trees")
-plt.ylabel("time to train")
-plt.scatter(num_trees, times_rfr, color='blue', label="rfr")
-plt.scatter(num_trees, times_scikit, color='red', label="scikit")
-
-plt.legend()
-plt.show()
+the_forest.fit(data,rng)
+alphas =[0.25, 0.5, 0.75]
+print("="*50)
+print(the_forest.predict_quantiles(feature_vector, alphas))
