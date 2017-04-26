777eac349bf97afbd55eea129d8363e3bdf62aea
biedenka <biedenka@informatik.uni-freiburg.de>
2017-04-26 17:01:35 +0200

    FIX setup.py to be installable via pip git+...

--- setup.py
+++ setup.py
@@ -1,33 +1,34 @@
+from distutils.command.build import build
+from setuptools.command.install import install
 from distutils.core import setup, Extension
-import distutils.command.build
-import numpy as np
-from subprocess import call
 
 
-
+class CustomInstall(install):
+	def run(self):
+		self.run_command('build_ext')
+		build.run(self)
+		self.do_egg_install()
 
 include_dirs = ['${CMAKE_SOURCE_DIR}/include', './include']
 extra_compile_args = ['-O2', '-std=c++11']
-#extra_compile_args = ['-g', '-std=c++11', '-O0']
+# extra_compile_args = ['-g', '-std=c++11', '-O0']
 
 
-
-extensions = [	Extension(
-					name = '_regression',
-					sources=['pyrfr/regression.i'],
-					include_dirs = include_dirs,
-					swig_opts=['-c++'] + ['-I{}'.format(s) for s in include_dirs],
-					extra_compile_args = extra_compile_args
-				),
-				Extension(
-					name = '_util',
-					sources=['pyrfr/util.i'],
-					include_dirs = include_dirs,
-					swig_opts=['-c++'] + ['-I{}'.format(s) for s in include_dirs],
-					extra_compile_args = extra_compile_args
-				)
+extensions = [Extension(
+						name='_regression',
+						sources=['pyrfr/regression.i'],
+						include_dirs=include_dirs,
+						swig_opts=['-c++'] + ['-I{}'.format(s) for s in include_dirs],
+						extra_compile_args=extra_compile_args
+					),
+					Extension(
+							name='_util',
+							sources=['pyrfr/util.i'],
+							include_dirs=include_dirs,
+							swig_opts=['-c++'] + ['-I{}'.format(s) for s in include_dirs],
+							extra_compile_args=extra_compile_args
+						)
 			]
-
 
 setup(
 	name='pyrfr',
@@ -37,5 +38,6 @@ setup(
 	license='Use as you wish. No guarantees whatsoever.',
 	classifiers=['Development Status :: 3 - Alpha'],
 	packages=['pyrfr'],
-	ext_modules=extensions
+	ext_modules=extensions,
+	cmdclass={'install': CustomInstall}
 )
