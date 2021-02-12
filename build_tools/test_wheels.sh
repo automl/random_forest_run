02c47be26ce51fe3844e61f0da8de43386d122df
chico <francisco.rivera.valverde@gmail.com>
2021-02-12 23:08:41 +0100

    project

--- build_tools/test_wheels.sh
+++ build_tools/test_wheels.sh
@@ -4,9 +4,7 @@ set -e
 set -x
 
 # test that we are able to load pyrfr
-pwd
-ls
-pytest tests/pyrfr_unit_test_binary_regression_forest_transformed_data.py
+pytest -v /project/tests/pyrfr_unit_test_binary_regression_forest_transformed_data.py
 
 # Test that there are no links to system libraries
 python -m threadpoolctl -i pyrfr
