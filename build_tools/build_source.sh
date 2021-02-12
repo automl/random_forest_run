5cb844c60ba5a9bb0b769836835606eb467ba6c9
chico <francisco.rivera.valverde@gmail.com>
2021-02-12 17:44:16 +0100

    build_tools/build_source.sh

--- build_tools/build_source.sh
+++ build_tools/build_source.sh
@@ -10,7 +10,10 @@ chmod u+x build_tools/build_package.sh
 # The version of the built dependencies are specified
 # in the pyproject.toml file, while the tests are run
 # against the most recent version of the dependencies
-cp ../../pyproject.toml .
+ls ../
+ls ../../
+ls ../../../
+cp ../../../pyproject.toml .
 
 # Also build the distribution
 python -m pip install twine
