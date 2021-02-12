fd138e1c65d0bde73e54c3c967b26449503a9750
chico <francisco.rivera.valverde@gmail.com>
2021-02-12 21:10:11 +0100

    yumaware

--- build_tools/build_wheels_env.sh
+++ build_tools/build_wheels_env.sh
@@ -1,12 +1,26 @@
-sudo apt-get install -y build-essential
-python -m pip install --upgrade pip
-sudo apt-get -qq update
-sudo apt-get install -y libboost-all-dev
-sudo apt-get remove swig
-sudo apt-get -y install swig3.0
-sudo ln -s /usr/bin/swig3.0 /usr/bin/swig
-sudo gem install coveralls-lcov
-sudo apt-get install -y lcov
-sudo apt-get install doxygen
-pip3 install --user -U pip-tools
+pip3 install cmake numpy==1.11.0 scipy==0.17.0
+echo 'echo "pyuic5 $@"' > /usr/local/bin/pyuic5
+chmod +x /usr/local/bin/pyuic5
+yum install -y curl gsl-devel pcre-devel
+curl -LO http://prdownloads.sourceforge.net/swig/swig-3.0.12.tar.gz
+tar xzvf swig-3.0.12.tar.gz
+cd swig-3.0.12
+./configure
+make
+make install
+cd ..
+rm -rf swig-3.0.12*
+curl -LO https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz
+tar xzvf boost_1_69_0.tar.gz
+cd boost_1_69_0
+./bootstrap.sh
+./b2 --without-atomic --without-chrono --without-container --without-context
+ --without-contract --without-coroutine --without-date_time --without-exception
+ --without-fiber --without-graph --without-graph_parallel --without-iostreams
+ --without-locale --without-log --without-math --without-mpi --without-python
+ --without-random --without-regex --without-stacktrace --without-thread
+ --without-timer --without-type_erasure --without-wave
+ install
+cd ..
+rm -rf boost_1_69_0*
 swig -version
