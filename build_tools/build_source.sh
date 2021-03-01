7e8238cef101cb33820f1aa73e8ec06c98c37fd8
chico <francisco.rivera.valverde@gmail.com>
2021-03-01 22:09:19 +0100

    Fixed build source

--- build_tools/build_source.sh
+++ build_tools/build_source.sh
@@ -14,19 +14,6 @@ cd build
 cmake .. && make pyrfr_docstrings
 cd python_package
 
-# Copy required files, which will be
-# available to a docker image that
-# will build wheel files
-# This docker image will copy the contents
-# of CWD into itself, so any build requirement
-# for wheels should be available
-# Directory Structure where docker image runs:
-# REPO/build/python_package/
-cp ../../pyproject.toml .
-cp -r ../../build_tools .
-# make the test data available for unit test
-cp -r ../../test_data_sets .
-
 # Also build the distribution
 python -m pip install twine
 python setup.py sdist -d ../../dist
