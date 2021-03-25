00270246e890978c5d1a53304519d776c090dffd
chico <francisco.rivera.valverde@gmail.com>
2021-03-25 15:00:15 +0100

    no hardcode swig

--- build_tools/env.sh
+++ build_tools/env.sh
@@ -9,9 +9,7 @@ python -m pip install --upgrade pip
 pip install "numpy<=1.19"
 sudo apt-get -qq update
 sudo apt-get install -y libboost-all-dev
-sudo apt-get remove swig
-sudo apt-get -y install swig3.0
-sudo ln -s /usr/bin/swig3.0 /usr/bin/swig
+sudo apt-get install -y swig
 sudo gem install coveralls-lcov
 sudo apt-get install -y lcov
 sudo apt-get install doxygen
